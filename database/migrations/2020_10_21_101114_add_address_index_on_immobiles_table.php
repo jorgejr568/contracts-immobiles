50d5fb2f8e206c607ed2c60fae22eae287829d80
Jorge Junior <jorge.junior.568@live.com>
2020-10-21 07:24:09 -0300

    feat: immobile address fields with index

---
+++ database/migrations/2020_10_21_101114_add_address_index_on_immobiles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAddressIndexOnImmobilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('immobiles', function (Blueprint $table) {
+            $table->index(['state', 'city', 'neighborhood', 'street', 'number']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('immobiles', function (Blueprint $table) {
+            $table->dropIndex(['state', 'city', 'neighborhood', 'street', 'number']);
+        });
+    }
+}
